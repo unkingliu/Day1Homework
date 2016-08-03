fd24d447fa800e736b58e3babdcdb53a1886d204
unkingliu <unking@gmail.com>
2016-08-03 13:43:59 +0800

    for HW1

---
+++ WebApplication1/Controllers/JournalController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class JournalController : Controller
+    {
+        // GET: Journal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult ChildAction_ListJ()
+        { var Journal = new List<JournalViewModel>
+            {
+            new JournalViewModel {jType=JournalType.支出, Amount=100, Date = new DateTime(2016,8,01), Remark="早餐" },
+            new JournalViewModel {jType=JournalType.收入, Amount=1200, Date = new DateTime(2016,8,02), Remark="零用金" },
+            new JournalViewModel {jType=JournalType.支出, Amount=140, Date = new DateTime(2016,8,03), Remark="咖啡" },
+            };
+            return View(Journal);
+        }
+    }
+}
---
+++ WebApplication1/ViewModels/JournalViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.ViewModels
+{
+    public class JournalViewModel
+    {
+        [Display(Name = "金額")]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public int Amount { get; set; }
+        [Display(Name = "日期")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        public DateTime Date { get; set; }
+        [Display(Name = "備註")]
+        public String Remark { get; set; }
+        [Display(Name = "類別")]
+        public JournalType jType { get; set; }
+    }
+
+    public enum JournalType { 收入,支出 }
+}
